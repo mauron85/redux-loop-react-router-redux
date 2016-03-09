41338f834fa146b206650778538024c4c86a38d2
Marian Hello <marian.hello@gmail.com>
2016-03-09 20:27:45 +0100

    fixing routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../containers/App';
 import MainPage from '../pages/Main';
+import OtherPage from '../pages/Other';
 
 export default () => (
   <Route path="/" component={App}>
     <IndexRoute component={MainPage}/>
-    <Route path="add/:trackingNr" component={MainPage}/>
+    <Route path="/other" component={OtherPage}/>
   </Route>
 );
